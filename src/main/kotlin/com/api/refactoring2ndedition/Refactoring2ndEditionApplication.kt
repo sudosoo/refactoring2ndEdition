09875491c4021d32421250b49994051be72e80bf
antidote <ljh8651@nate.com>
2025-01-20 16:56:37 +0900

    init

---
+++ src/main/kotlin/com/api/refactoring2ndedition/Refactoring2ndEditionApplication.kt
@@ -0,0 +1,11 @@
+package com.api.refactoring2ndedition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Refactoring2ndEditionApplication
+
+fun main(args: Array<String>) {
+    runApplication<Refactoring2ndEditionApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/refactoring2ndedition/Refactoring2ndEditionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.refactoring2ndedition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Refactoring2ndEditionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
